a2e84f4ff15f41afc30eb054064c1675663f4df9
zudikhin <uzudihin@gmail.com>
2025-10-28 23:21:28 +0300

    empties

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
   "use strict";
 
-
+  $(".header_wrap_bottom_burger").click(function() {
+    $(".drodown").addClass("active");
+  });
   
+  $(".drodown_top_close").click(function() {
+    $(".drodown").removeClass("active");
+  });
+
+  $(".drodown_cats .drop").click(function() {
+    var attr = $(this).attr("data-target");
+    $(".drodown").addClass("drop");
+    $(`#${attr}`).addClass("active");
+  });
+
+  $(".drodown_item_back").click(function() {
+    $(".drodown").removeClass("drop");
+    $(".drodown_item").removeClass("active");
+  });
+
+  $('.header_catalog_wrap_left li').on('mouseenter', function() {
+    var targetId = $(this).data('target');
+
+    $('.header_catalog_wrap_left li').removeClass('active');
+    $('.header_catalog_wrap_right_item').removeClass('active');
+
+    $(this).addClass('active');
+    $('#' + targetId).addClass('active');
+  });
+
+  $(".header_wrap_top_catalog").click(function() {
+    $(this).toggleClass("active");
+    $(".header_catalog").toggleClass("active");
+  });
 
 });
