8924481b37720d8ae7e8a171e70eb1d4656405e6
zudikhin <uzudihin@gmail.com>
2025-10-30 23:12:35 +0300

    pages

--- js/main.js
+++ js/main.js
@@ -35,4 +35,61 @@ $(document).ready(function() {
     $(".header_catalog").toggleClass("active");
   });
 
+  $('.main_slider_wrap').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    cssEase: 'linear',
+    centerMode: true,
+    variableWidth: true,
+    arrows: true,
+    fade: false,
+    prevArrow: $('.main_slider_arrows_prev'),
+    nextArrow: $('.main_slider_arrows_next'),
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          centerMode: false,
+          variableWidth: false,
+          fade: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $(".contacts_nav_wrap_item").click(function() {
+    var attr = $(this).attr("data-target");
+    $(".contacts_nav_wrap_item").removeClass("active");
+    $(this).addClass("active");
+    $(".contacts_item").removeClass("active");
+    $(`#${attr}`).addClass("active");
+  });
+
+  $('.show').on('click', function() {
+    const $this = $(this);
+    const $input = $this.siblings('input');
+    if ($input.attr('type') === 'password') {
+      $input.attr('type', 'text');
+      $this.addClass('active');
+    } else {
+      $input.attr('type', 'password');
+      $this.removeClass('active');
+    }
+  });
+
+
+  $('input[type=tel], input.phone').each(function(){
+    $(this)
+      .attr('placeholder', '+7 (___) ___-__-__')
+      .mask('+7 (000) 000-00-00', {
+        autoclear: false
+      });
+  });
+
 });
